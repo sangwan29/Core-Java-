2f333525790452808a6eb223ef240a68ced935a1
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-10 23:06:07 +0530

    Entering value by user in Double datatyep

---
+++ basic/UserDouble.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class UserDouble{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the decimal value:");
+        double num=sc.nextDouble();
+        System.out.println("You entered value is:"+num);
+    }
+}
