9cd2b3a50c809f7a9dda11177ed4187c73bee207
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-10 22:56:06 +0530

    Entering value by user in Long datatype program

---
+++ basic/UserLong.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class UserLong{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        long num = sc.nextLong();
+        System.out.println("You entered value is:"+num);
+    }
+}
