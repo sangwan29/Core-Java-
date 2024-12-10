352743ef7a55d4134b67078e7556bb504db47217
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-10 22:07:59 +0530

    Java basic program of using arithmetic operator in java

---
+++ basic/Program6.java
@@ -0,0 +1,9 @@
+//3.4+1.2*(3.7-1.2)/2+6.8
+public class Program6{
+    public static void main(String[] args) {
+        float a = 3.4f,b=1.2f,c=3.7f,d=1.2f,e=6.8f,g;
+        int f=2;
+        g=a+b*(c-d)/f+e;
+        System.out.println("The solution is:"+g);
+    }
+}
