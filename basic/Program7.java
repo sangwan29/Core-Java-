2e62c65bcd6ad810b82f533e628c71aa30c5e4f5
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-10 22:13:53 +0530

    Basic program of java

---
+++ basic/Program7.java
@@ -0,0 +1,9 @@
+//8.9*1.67-(6.21-5.6)+7.8-34 +157
+public class Program7{
+    public static void main(String[] args){
+        float a=8.9f,b=1.67f,c=6.21f,d=5.6f,e=7.8f,h;
+        int f=34,g=157;
+        h=a*b-(c-d)+e*f+g;
+        System.out.println("The solution is:"+h);
+    }
+}
