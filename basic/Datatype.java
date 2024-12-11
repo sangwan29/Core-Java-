fd8d8b87b32ca8f59f0ab4b1ab838b684fe93eb2
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-11 16:03:13 +0530

    All premitive data types in one print

---
+++ basic/Datatype.java
@@ -0,0 +1,13 @@
+public class Datatype{
+    public static void main(String[] args){
+        byte a=5;
+        short b=458;
+        int c=45678;
+        float d=45.2f;
+        double e=4589.256;
+        char f='f';
+        boolean g=true;
+        long h=4578921;
+        System.out.print("byte: "+a+" Short:"+b+" Int:"+c+" Float:"+d+" double:"+e+" Char:"+f+" Boolean"+g+" Long:"+h);
+    }
+}
