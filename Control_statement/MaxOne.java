fcdea4a6655329b0af714362b3c50a336478f935
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-12 15:29:29 +0530

    Program for check the number enter by user is greater than 10

---
+++ Control_statement/MaxOne.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MaxOne{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter one number:");
+        int a=sc.nextInt();
+        if(a>10){
+            System.out.println("a is greater than 10");
+        }
+    }
+}
