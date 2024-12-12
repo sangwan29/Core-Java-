83a2e30bf4a066abaf3ab3b3a34711f2b9f71729
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-12 23:41:11 +0530

    Program for check the character enter by user is vowel and consonant

---
+++ Control_statement/Vowel_consonant.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Vowel_consonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character:");
+        char ch=sc.next().charAt(0);
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+    }
+}
