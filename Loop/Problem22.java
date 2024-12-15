8e34ff758a37410757607434bae305e6c5fea6fb
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-15 14:21:28 +0530

    Program for print evan number from 1 to 100 using while loop

---
+++ Loop/Problem22.java
@@ -0,0 +1,11 @@
+public class Problem22 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=100){
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
