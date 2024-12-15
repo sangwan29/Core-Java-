7b61939e99516c00a79d54b14f90d62ccb9b947c
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-16 00:04:19 +0530

    Print table of 2 in this format 2 X 1 =2 using for loop

---
+++ Loop/For_problem12.java
@@ -0,0 +1,8 @@
+public class For_problem12 {
+    public static void main(String[] args) {
+        int n=2;
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" x "+i + " = " + n*i);
+        }
+    }
+}
