a3f08a671c37e868ffaf0bb1740fc1fed4a39604
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-16 19:30:05 +0530

    Program for print pattern in java

---
+++ Loop/Pattern22.java
@@ -0,0 +1,15 @@
+public class Pattern22 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=5;j++){
+                if ((i + j) % 2 == 0) {
+                    System.out.print("0");
+                } 
+                else {
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
