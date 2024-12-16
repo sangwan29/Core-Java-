914520b2babf18c6acecdf39e9f4cb4e18dd874b
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-16 22:59:47 +0530

    Pattern program

---
+++ Loop/Pattern43.java
@@ -0,0 +1,15 @@
+public class Pattern43 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(j==1||j==3||j==5){
+                    System.out.print(0+" ");
+                }
+                else{
+                    System.out.print(1+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
