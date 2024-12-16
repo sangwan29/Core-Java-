2c02612f8cfb27fce0c1d5915842eb4a6027bf2d
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-16 18:31:58 +0530

    Program for print star pattern

---
+++ Loop/Pattern15.java
@@ -0,0 +1,24 @@
+public class Pattern15 {
+    public static void main(String[] args) {
+        int rows = 3; 
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
