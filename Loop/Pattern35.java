d99cdf4136db53a9dbbd2ed2992272b47c92854e
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-16 21:44:33 +0530

    Pattern program

---
+++ Loop/Pattern35.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Pattern35 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int rows=sc.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i*i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
