608f7784450c8df320a8d2d477b7e66fcd5ed1f1
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-16 15:59:26 +0530

    Program for print pattern using while loop in java

---
+++ Loop/While_pattern2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class While_pattern2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter starting value: ");
+        int start=sc.nextInt();
+        System.out.print("Enter ending value: ");
+        int end=sc.nextInt();
+        int i=start;
+        while(i<=end){
+            int j=1;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
