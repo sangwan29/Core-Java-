9430dbb42d2ebe2b2a12d340030528df2658c25d
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-16 18:55:26 +0530

    Program for print pattern in java

---
+++ Loop/Pattern19.java
@@ -0,0 +1,12 @@
+public class Pattern19 {
+    public static void main(String[] args) {
+        int k=1;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
