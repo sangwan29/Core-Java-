f47879ab44b0a480ff358731774fe293406de2da
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-19 15:56:20 +0530

    Method of basic in java

---
+++ OOPs/Methods/Method1.java
@@ -0,0 +1,11 @@
+class A{
+    void show(){
+        System.out.println("Hello World");
+    }
+}
+public class Method1 {
+    public static void main(String[] args) {
+        A p=new A();
+        p.show();
+    }
+}
