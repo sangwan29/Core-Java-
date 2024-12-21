03860476b8c246c7c044ee303e129864cb345a24
sangwan29 <sangwandivya03@gmail.com>
2024-12-22 01:25:01 +0530

    Program for enter variable

---
+++ Basic_Java/Basic/Test/Variable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Variable {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age=sc.nextInt();
+        System.out.println("My age is:"+age);
+        System.out.println("Enter price");
+        double price=sc.nextDouble();
+        System.out.println("Price is:"+price);
+    }
+}
