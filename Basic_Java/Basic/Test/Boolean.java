690cc1cb19a345d986ee4e0c2aac6ff077661b95
sangwan29 <sangwandivya03@gmail.com>
2024-12-24 01:02:47 +0530

    Program for boolean datatype

---
+++ Basic_Java/Basic/Test/Boolean.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Boolean {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the boolean value");
+        boolean isJavaFun=sc.nextBoolean();
+        System.out.println("Enter the boolean value:");
+        boolean isCodingEasy=sc.nextBoolean();
+        System.out.println("The value of isJavaFun: "+isJavaFun);
+        System.out.println("The value of isCodingEasy: "+isCodingEasy );
+    }
+}
