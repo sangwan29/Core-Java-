6bcd8eaf7ec8d6e4107d9331c0167d2cd1942b66
sangwan29 <sangwandivya03@gmail.com>
2024-12-24 23:54:29 +0530

    Write a java program that prints all even numbers between 1 and 50 (inclusive).

---
+++ Basic_Java/Basic/Java_Questions/Even.java
@@ -0,0 +1,11 @@
+public class Even {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=50) {
+            if(i%2==0) {
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
