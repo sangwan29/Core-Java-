a26d30b0e8e38cb91a731bcd298cb34e64d6df82
sangwan29 <sangwandivya03@gmail.com>
2024-12-25 20:19:25 +0530

    Write a java program to find all factors of a number.

---
+++ Basic_Java/Basic/Java_Questions/FindFactors.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FindFactors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("The factors of " + number + " are:");
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
+
