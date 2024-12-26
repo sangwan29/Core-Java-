3225a9defd74b4b336d53fef96811fda1ba3a618
sangwan29 <sangwandivya03@gmail.com>
2024-12-26 15:25:33 +0530

    Program for check the year is leap or not

---
+++ Basic_Java/Control_Statement/Leap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Leap{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+            System.out.println("Enter the year:");
+            int year=sc.nextInt();
+            if(year%4==0){
+                System.out.println(year+" is a Leap year");
+            }
+            else{
+                System.out.println("Not a leap year");
+            }
+
+    }
+}
