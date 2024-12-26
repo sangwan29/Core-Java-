6ad1fc0e7b7da7ac215b61ecd3396dc6c309a43c
sangwan29 <sangwandivya03@gmail.com>
2024-12-26 15:29:11 +0530

    Program for check the number is maximum between two numbers

---
+++ Basic_Java/Control_Statement/MaxTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MaxTwo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number");
+        int b=sc.nextInt();
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+    }
+}
