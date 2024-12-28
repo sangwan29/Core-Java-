fa880203aadffae2d5b3bac4158d92e2255a9a0f
sangwan29 <sangwandivya03@gmail.com>
2024-12-28 19:02:38 +0530

    Write a java program to print all even numbers between 1 to 100. - using while loop

---
+++ Basic_Java/Basic/Java_Questions/While_problem4.java
@@ -0,0 +1,11 @@
+public class While_problem4 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=100){
+            if(i%2==0){
+                System.out.print(i+ " ");
+            }
+            i++;
+        }
+    }
+}
