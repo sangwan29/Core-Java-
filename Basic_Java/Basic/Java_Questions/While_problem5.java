b38652d87af5fc6b154f3fdf53603d3094cba930
sangwan29 <sangwandivya03@gmail.com>
2024-12-28 19:06:58 +0530

    Write a java program to print all odd number between 1 to 100.

---
+++ Basic_Java/Basic/Java_Questions/While_problem5.java
@@ -0,0 +1,11 @@
+public class While_problem5 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100){
+            if(i%2!=0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
