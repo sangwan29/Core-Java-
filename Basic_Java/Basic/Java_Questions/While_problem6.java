a23dc66b3e71e57536bf7a1a55063548fb2d3a1a
sangwan29 <sangwandivya03@gmail.com>
2024-12-28 19:10:33 +0530

    Write a java program to find sum of all natural numbers between 1 to n.

---
+++ Basic_Java/Basic/Java_Questions/While_problem6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class While_problem6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n=sc.nextInt();
+        int i=1;
+        int sum=0;
+        while(i<=n) {
+            sum=sum+i;
+            i++;
+        }
+        System.out.println("Sum of first "+n+" natural numbers is: "+sum);
+    }
+}
