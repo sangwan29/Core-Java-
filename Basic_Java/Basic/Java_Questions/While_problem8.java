67b1a55a1e00bde1aba79fc09c6dc8e31d510475
sangwan29 <sangwandivya03@gmail.com>
2024-12-28 19:15:40 +0530

    Write a java program to find sum of all odd numbers between 1 to n

---
+++ Basic_Java/Basic/Java_Questions/While_problem8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class While_problem8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+        while (i <= n) {
+            if(i%2!=0){
+                sum = sum + i;
+            }
+            i++;
+        }
+        System.out.println("Sum of odd numbers is: "+sum);
+    }
+}
