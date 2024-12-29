9173415be6202f33f938c5f38feb384604e8425a
sangwan29 <sangwandivya03@gmail.com>
2024-12-29 18:46:34 +0530

    Write a java program to print all alphabets from a to z. - using while loop

---
+++ Basic_Java/Basic/Java_Questions/While_Problem3.java
@@ -0,0 +1,11 @@
+public class While_Problem3 {
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println("Alphabets from a to z:");
+        while (ch <= 'z') {
+            System.out.print(ch + " ");
+            ch++;
+        }
+    }
+}
+
