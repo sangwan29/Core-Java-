2deef0d2e41bbea250411338fed19f32dfff74e3
sangwan29 <sangwandivya03@gmail.com>
2025-02-10 23:30:34 +0530

    Program for check the number id greater than 10

---
+++ Basic_Java/Control_Statement/IfElseMax1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class IfElseMax1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number who find maximum: ");
+        int n=sc.nextInt();
+        if(n>10){
+            System.out.println("The number is greater than 10");
+        }
+    }
+}
